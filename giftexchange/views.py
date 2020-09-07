4a8b10ad3530c56ae8137907f83f9b30236e2d43
Bo Morin <morinbe@gmail.com>
2020-09-07 09:24:51 -0400

    Work out some UX issues with authenticated base view

--- giftexchange/views.py
+++ giftexchange/views.py
@@ -1,12 +1,15 @@
 from datetime import date
+from urllib.parse import urlparse
+
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, REDIRECT_FIELD_NAME
 from django.template import loader
 from django.http import HttpResponse, Http404
-from django.shortcuts import redirect
+from django.shortcuts import redirect, resolve_url
 from django.urls import reverse
 from django.contrib.auth.models import User
+from django.contrib.auth.views import redirect_to_login
 from django.views import View
 
 from giftexchange.forms import (
@@ -129,11 +132,64 @@ class BaseView(View):
 class AuthenticatedView(BaseView):
 	""" Base class thay boots you from a view if you are not logged in
 		also does some other variable assignments that are commonly required
+
+		Wow LoginRequiredMixin was not working at all so I had to copy a bunch of it here :\
 	"""
+	login_url = '/login/'
+	permission_denied_message = 'Login required'
+	raise_exception = False
+	redirect_field_name = REDIRECT_FIELD_NAME
+
+	def get_login_url(self):
+		"""
+		Override this method to override the login_url attribute.
+		"""
+		login_url = self.login_url or settings.LOGIN_URL
+		if not login_url:
+			raise ImproperlyConfigured(
+				'{0} is missing the login_url attribute. Define {0}.login_url, settings.LOGIN_URL, or override '
+				'{0}.get_login_url().'.format(self.__class__.__name__)
+			)
+		return str(login_url)
+
+
+	def get_redirect_field_name(self):
+		"""
+		Override this method to override the redirect_field_name attribute.
+		"""
+		return self.redirect_field_name
+
+	def handle_no_permission(self):
+		if self.raise_exception or self.request.user.is_authenticated:
+			raise PermissionDenied(self.get_permission_denied_message())
+
+		path = self.request.build_absolute_uri()
+		resolved_login_url = resolve_url(self.get_login_url())
+		# If the login url is the same scheme and net location then use the
+		# path as the "next" url.
+		login_scheme, login_netloc = urlparse(resolved_login_url)[:2]
+		current_scheme, current_netloc = urlparse(path)[:2]
+		if (
+			(not login_scheme or login_scheme == current_scheme) and
+			(not login_netloc or login_netloc == current_netloc)
+		):
+			path = self.request.get_full_path()
+			return redirect_to_login(
+				path,
+				resolved_login_url,
+				self.get_redirect_field_name(),
+			)
+
+	def dispatch(self, request, *args, **kwargs):
+		if not request.user.is_authenticated:
+			return self.handle_no_permission()
+		return super().dispatch(request, *args, **kwargs)
+
 	def setup(self, request, *args, **kwargs):
 		super(AuthenticatedView, self).setup(request, *args, **kwargs)
-		if not self.request.user.is_authenticated:
-			raise Http404('Login Required')
+
+		if not request.user.is_authenticated:
+			return self.handle_no_permission()
 
 		self.giftexchange = None
 		self.is_admin = False
@@ -367,6 +423,7 @@ class GiftExchangeDetailEdit(GiftExchangeView):
 				dislikes=request.POST['dislikes'],
 				allergies_sensitivities=request.POST['allergies_sensitivities'],
 			)
+			messages.success(request, 'Updated your details for this gift exchange')
 			return redirect(reverse('giftexchange_detail', kwargs={'giftexchange_id': self.giftexchange_id}))
 		else:
 			form = ParticipantDetailsForm(instance=self.participant_details)
