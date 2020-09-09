43a3a731c6efc8b8480c4f462347be2269cce4d5
Bo Morin <morinbe@gmail.com>
2020-09-09 11:31:20 -0400

    Add heroku deploy things

---
+++ project/settings_production.py
@@ -0,0 +1,23 @@
+import os
+from project.settings import *  # noqa
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['DATABASE_NAME'],
+        'USER': os.environ['DATABASE_USER'],
+        'PASSWORD': os.environ['DATABASE_PASSWORD'],
+        'HOST': os.environ['DATABASE_HOST'],
+        'PORT': '5432'
+    }
+}
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ['GMAIL_USER']
+EMAIL_HOST_PASSWORD = os.environ['GMAIL_PASS']
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+ALLOWED_HOSTS = ['gifterator3000.herokuapp.com', 'herokuapp.com', ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
